12069fec72cca309c381336496daef548a59f4db
Yuseung Lee <phillip9807@gmail.com>
2022-05-13 13:14:43 +0000

    define class ImageDecomp()

--- unsup3d/networks.py
+++ unsup3d/networks.py
@@ -1,7 +1,43 @@
 '''
-
+Define each network for the Photo-geometric Autoencoding pipeline.
 '''
-from .modules import Encoder, AutoEncoder, Conf_Conv
+from modules import Encoder, AutoEncoder, Conf_Conv
+
+# Image decompostion pipline
+class ImageDecomp():
+    def __init__(self, depth_v='v0', alb_v='v0', light_v='v0', view_v='v0'):
+        if depth_v == 'v0':
+            self.depth_net = AutoEncoder(1)         # B x 1 x W x H
+        
+        ''' TODO: additional depth networks'''
+        
+        if alb_v == 'v0':
+            self.alb_net = AutoEncoder(3)           # B x 3 x W x H
+
+        ''' TODO: additional albedo networks'''
+
+        if light_v == 'v0':
+            self.light_net = Encoder(4)             # B x 4 x 1 x 1
+        
+        ''' TODO: additional light networks '''
+        
+        if view_v == 'v0':
+            self.view_net = Encoder(6)              # B x 4 x 1 x 1
+
+        ''' TODO: additional view networks '''
+
+    def get_depth_map(self, input):
+        return self.depth_net(input)
+    
+    def get_albedo(self, input):
+        return self.alb_net(input)
+
+    def get_light(self, input):
+        return self.light_net(input)
+
+    def get_view(self, input):
+        return self.view_net(input)
+
 
 class DepthNet_v1():
     def __init__(self):
