303033a336207b21bb8f4424d0c3d770a5268fd5
Yuseung Lee <phillip9807@gmail.com>
2022-05-31 16:04:02 +0000

    save last epoch

--- unsup3d/train.py
+++ unsup3d/train.py
@@ -136,7 +136,7 @@ class Trainer():
                 self.save_model(epch_loss)
                 self.best_loss = epch_loss
 
-            if self.epoch % self.save_epoch == 0:
+            if self.epoch % self.save_epoch == 0 or self.epoch == (self.max_epoch - 1):
                 # save periodically
                 self.save_model(epch_loss)
 
